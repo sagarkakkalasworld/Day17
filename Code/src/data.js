d9e9f595f5db09c04829b455b5806298085e67d5
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-04-26 12:43:21 +0100

    Update data.js

--- Code/src/data.js
+++ Code/src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "ArgoCD has been successfully configured",
+    title: "ArgoCD Demo successfully configured",
     desc: "This is now in new commit",
     bg: "green",
   },
